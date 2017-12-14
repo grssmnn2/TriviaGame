3c91ba0bda3e90f109fc2c4c86e948a3e84b5c18
Cecily Grossmann <grssmnn2@gmail.com>
2017-12-14 10:22:01 -0600

    added timer, new div to hold timer and text

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,5 @@
-$(document).ready(function(){
-    // inital correct/incorrect guesses start at 0
+$(document).ready(function () {
+    // variables for questions to be edited later
     var weasley;
     var hippogriff;
     var albus;
@@ -7,51 +7,68 @@ $(document).ready(function(){
     var lady;
     var animagus;
     var score;
+    // sounds to play on page/when buttons are clicked
     var harryTheme = new Audio("assets/images/harrytheme.mp3");
     var alohomora = new Audio("assets/images/alohomora.mp3");
     // play Harry Potter Theme on page load
     harryTheme.play();
 
-// initial page has game and score screens hidden and start screen showing
+    // initial page has game and score screens hidden and start screen showing
     $(".gameScreen").hide();
     $(".startScreen").show();
     $(".scoreScreen").hide();
 
-// once start button is clicked, hide start screen and show game screen. start countdown timer.
-$(".start").on("click", function(){
-    alohomora.play();
-    $(".startScreen").hide();
-    $(".gameScreen").show();
-    $(".scoreScreen").hide();
-});
-// keep track of answers based on user selection, make sure user can only select one box per guess
+    // once start button is clicked, hide start screen and show game screen. start countdown timer.
+    $(".start").on("click", function () {
+        alohomora.play();
+        $(".startScreen").hide();
+        $(".gameScreen").show();
+        $(".scoreScreen").hide();
+        var timer = setInterval(function () {
+            // Get todays date and time
+            var now = new Date().getTime();
+            // make timer subtract current seconds from 30
+            var timeLeft = (now-1);
+            //give user 30 seconds to take quiz
+            var seconds = Math.floor((timeLeft % (1000 * 30)) / 1000);
+          
+            // display time remaining on user screen
+            $("#timer").text("Time Left to Turn in: " + seconds);
+            if(timeLeft <= 0){
+                clearInterval(timer);
+                $("#timer").text("It takes a bit more studying than that to be an exemplary witch or wizard.");
+            }
+        }, 1000);
 
-// after submit button is clicked, hide game and start screen and show score screen.
-$(".submit").on("click", function(){
-    $(".startScreen").hide();
-    $(".gameScreen").hide();
-    $(".scoreScreen").show();
-});
+        // keep track of answers based on user selection, make sure user can only select one box per guess
+        $("#harryQuiz").onsubmit(function () {
+            weasley = parseInt($('input[name="weasley"]:checked').value);
+            hippogriff = parseInt($('input[name="hippogriff"]:checked').value);
+            animagus = parseInt($('input[name="animagus"]:checked').value);
+            albus = parseInt($('input[name="albus"]:checked').value);
+            candy = parseInt($('input[name="candy"]:checked').value);
+            lady = parseInt($('input[name="lady"]:checked').value);
+            // add total number of correct answers together based on values given in html
+            score = weasley + hippogriff + animagus + albus + candy + lady;
 
-// when user clicks restart button, reload game.
-$(".restart").on("click", function(){
-    $(".gameScreen").hide();
-    $(".startScreen").show();
-    $(".scoreScreen").hide();
-});
+        });
+    });
 
-$("#harryQuiz").onsubmit(function(){
-    weasley = parseInt($('input[name="weasley"]:checked').value);
-    hippogriff = parseInt($('input[name="hippogriff"]:checked').value);
-    animagus = parseInt($('input[name="animagus"]:checked').value);
-    albus = parseInt($('input[name="albus"]:checked').value);
-    candy = parseInt($('input[name="candy"]:checked').value);
-    lady = parseInt($('input[name="lady"]:checked').value);
+    // after submit button is clicked, hide game and start screen and show score screen.
+    $(".submit").on("click", function () {
+        $(".startScreen").hide();
+        $(".gameScreen").hide();
+        $(".scoreScreen").show();
 
-    score = weasley + hippogriff + animagus + albus + candy + lady;
+        $(".scoreBox").html("You correctly answered" + score + " questions on your O.W.L.");
+    });
 
-    $(".scoreBox").html("You correctly answered" + score + " questions correctly on your O.W.L.");
-});
+    // when user clicks restart button, reload game.
+    $(".restart").on("click", function () {
+        $(".gameScreen").hide();
+        $(".startScreen").show();
+        $(".scoreScreen").hide();
+    });
 
 
 });
