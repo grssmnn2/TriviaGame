accd51f0500f4b2a7d091cec4b85791ebfefbfcd
Cecily Grossmann <grssmnn2@gmail.com>
2018-06-02 19:27:58 -0500

    code cleanup

--- README.md
+++ README.md
@@ -4,8 +4,5 @@
 
 ## Make guesses from selected answers to complete your O.W.L. with various responses from Professor Snape depending on your score.
 
-## Utilizes JavaScript, jQuery, HTML5, and CSS3
+#### Utilizes JavaScript, jQuery, HTML5, and CSS3
 
-### Live Link:
-
-#### https://grssmnn2.github.io/TriviaGame/
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,19 +1,7 @@
 $(document).ready(function () {
-    // variables for questions to be edited later
-    var weasley;
-    var hippogriff;
-    var albus;
-    var candy;
-    var lady;
-    var animagus;
-    // variables for score and timer which update based on specific game
-    var score;
-    var incorrect;
-    var timer;
     // sounds to play on page/when buttons are clicked
     var harryTheme = new Audio("assets/images/harrytheme.mp3");
     var alohomora = new Audio("assets/images/alohomora.mp3");
-    // play Harry Potter Theme on page load
     harryTheme.play();
 
     // initial page has game and score screens hidden and start screen showing
@@ -22,10 +10,9 @@ $(document).ready(function () {
     $(".scoreScreen").hide();
     $(".failScreen").hide();
 
-    // once start button is clicked, hide start screen and show game screen. start countdown timer.
+    var timer;
     $(".start").on("click", function () {
-        alohomora.play();
-        //  $('input[[name=lady], [name=candy], [name=animagus], [name=hippogriff], [name=albus], [name=weasley]]').attr('checked',false);
+        // alohomora.play();
         $(".startScreen").hide();
         $(".gameScreen").show();
         $("body").css("background-image", "url(assets/images/quidditch.jpg)");
@@ -52,15 +39,15 @@ $(document).ready(function () {
 
     // keep track of answers based on user selection, make sure user can only select one box per guess
     document.getElementById("harryQuiz").onsubmit = function () {
-        weasley = parseInt(document.querySelector('input[name="weasley"]:checked').value);
-        hippogriff = parseInt(document.querySelector('input[name="hippogriff"]:checked').value);
-        animagus = parseInt(document.querySelector('input[name="animagus"]:checked').value);
-        albus = parseInt(document.querySelector('input[name="albus"]:checked').value);
-        candy = parseInt(document.querySelector('input[name="candy"]:checked').value);
-        lady = parseInt(document.querySelector('input[name="lady"]:checked').value);
+        var weasley = parseInt(document.querySelector('input[name="weasley"]:checked').value);
+        var hippogriff = parseInt(document.querySelector('input[name="hippogriff"]:checked').value);
+        var animagus = parseInt(document.querySelector('input[name="animagus"]:checked').value);
+        var albus = parseInt(document.querySelector('input[name="albus"]:checked').value);
+        var candy = parseInt(document.querySelector('input[name="candy"]:checked').value);
+        var lady = parseInt(document.querySelector('input[name="lady"]:checked').value);
         // add total number of correct and incorrect answers together based on values given in html
-        score = weasley + hippogriff + animagus + albus + candy + lady;
-        incorrect = (6 - score);
+        var score = weasley + hippogriff + animagus + albus + candy + lady;
+        var incorrect = (6 - score);
 
         // display correct number of guesses in scoreBox div
         $(".scoreBox").html("You answered " + score + " questions correctly and " + incorrect + " incorrectly on your O.W.L.");
